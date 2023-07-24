f3eef61504981a4c147d193f2695ec9f6f65b1a9
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2023-07-24 11:26:39 -0700

    updated import data as RDS format instead of original data with data wrangling scripts, RDS format is of the original data that has undergone data wrangling, refer to data script for code

--- scripts/.giant anteater.R
+++ scripts/.giant anteater.R
@@ -30,41 +30,13 @@ library(corrMove)        #correlative movement
 #............................................................
 
 # Set working directory
-setwd("C:/Users/Kat/Documents/GitHub/giant anteater")
+setwd("C:/Users/achhen/Documents/GitHub/Giant_Anteater")
 
 #import data, cleaned GPS giant anteater data
-DATA_GPS <- read_csv("data/Anteaters_NoOutliers.csv")
-
-#correct a mismatch entry for 'Larry' to 'Larry 267'
-DATA_GPS$ID[DATA_GPS$ID == "Larry 267"] <- "Larry"
-DATA_GPS$ID[DATA_GPS$ID == "Little Rick"] <- "Little_Rick"
-#subset to 23 range-resident individuals
-DATA_GPS <- DATA_GPS[which(DATA_GPS$ID %in% c("Alexander", "Annie", "Anthony", "Beto", "Bumpus",
-                                              "Cate", "Christoffer","Elaine", "Hannah","Jackson",
-                                              "Jane","Kyle", "Larry", "Little_Rick", "Luigi",
-                                              "Makao", "Margaret", "Maria", "Puji", "Reid", 
-                                              "Rodolfo", "Sheron", "Thomas")),]
-
-# Convert dataset to a telemetry object
-DATA_TELEMETRY <- as.telemetry("data/Anteaters_NoOutliers.csv")
-
-#correct a mismatch entry for 'Larry' to 'Larry 267'
-names(DATA_TELEMETRY)[25] <- "Larry"
-names(DATA_TELEMETRY)[27] <- "Little_Rick"
-
-#subset to 23 range-resident individuals
-DATA_TELEMETRY  <- DATA_TELEMETRY[c("Alexander", "Annie", "Anthony", "Beto", "Bumpus",
-                                    "Cate", "Christoffer","Elaine", "Hannah","Jackson",
-                                    "Jane","Kyle", "Larry", "Little_Rick", "Luigi",
-                                    "Makao", "Margaret", "Maria", "Puji", "Reid", 
-                                    "Rodolfo", "Sheron", "Thomas")]
-
-# Import supplementary data containing biological information
-DATA_META <- read_csv("data/Anteater_Results_Final.csv")
-#subset to 23 range-resident individuals
-DATA_META <- DATA_META[c(1:3,8:10,12,14,17,19,20,22,23,25:29,33:35,37,38),]
-DATA_META$ID[DATA_META$ID == "Little Rick"] <- "Little_Rick"
-DATA_BIO <- DATA_META[,1:3]
+DATA_GPS <- readRDS("RDS/DATA_GPS.RDS")
+DATA_TELEMETRY <- readRDS("RDS/DATA_TELEMETRY.RDS")
+DATA_META <- readRDS("RDS/DATA_META.RDS")
+DATA_BIO <- readRDS("RDS/DATA_BIO.RDS")
 
 #............................................................
 # Home range ----
