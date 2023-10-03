539eebfaeaef23895f95411caebfd1fe287cc5b7
Kat <katchhen@gmail.com>
2023-10-03 08:37:32 +0700

    corrected typo for a yaxis label

--- scripts/figure3_proximity_encounters.R
+++ scripts/figure3_proximity_encounters.R
@@ -1,9 +1,9 @@
 
-# Proximity ratio and encounters
 # Figure 3
+# Proximity and encounters
 
 #..................................................
-## Proximity ratio: Figure 3 A  ----
+## Figure 3A: Proximity ratio ----
 #..................................................
 
 figure3a_proximity_ratio <- 
@@ -46,20 +46,21 @@ figure3a_proximity_ratio <-
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) #top, right, bot, left
 figure3a_proximity_ratio
-ggsave(figure3a_proximity_ratio, width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent",
-       file="figures/individual figures/figure3a_proximity_ratio.png")
+ggsave(figure3a_proximity_ratio, 
+       file="figures/individual figures/figure3a_proximity_ratio.png",
+       width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent")
 
 #..................................................
-## Encounters: Figure 3 B ----
+# Figure 3B: Encounters ----
 #..................................................
 
 #do not include 0 encounters
-overlap_df2 <- overlap_df[overlap_df$encounter_count != 0,]
+proximity_df2 <- proximity_df[proximity_df$encounter_count != 0,]
 
 figure3b_encounters <-
-  ggplot(data = overlap_df2,
+  ggplot(data = proximity_df2,
          aes(y = encounter_count, x = overlap_est)) +
-  geom_point(data = overlap_df2, 
+  geom_point(data = proximity_df2, 
              aes(y = encounter_count, x = overlap_est, col = sex_comparison),
              size = 1.2) + 
   geom_smooth(method="lm", formula = y ~ x, se=F, col = "black") +
@@ -91,26 +92,95 @@ figure3b_encounters <-
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) #top, right, bot, left
 figure3b_encounters
-ggsave(figure3b_encounters, width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent",
-       file="figures/individual figures/figure3b_encounters.png")
+ggsave(figure3b_encounters,
+       file="figures/individual figures/figure3b_encounters.png",
+       width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent")
 
 #..................................................
-## Encounters over time: Figure 3 C ----
+# Figure 3C: Encounters over time ----
 #..................................................
 
-#encounters over time
+#set threshold of 15m
 distance_df$encounter <- ifelse(distance_df$distance_est > 15, 0,1)
 encounter_df <- distance_df[which(distance_df$encounter == 1),]
 encounter_df$doy <- yday(encounter_df$timestamp) #day of the year
 encs <- aggregate(encounter ~ sex_comparison + doy + pair_ID, data = encounter_df, FUN = "sum")
 encs <- merge(encs, overlap_df[,c("pair_ID","sex_comparison", "site")])
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # OLD FIGURE
+# figure3c_encounters_overtime <-
+#   ggplot(data = encs,
+#          aes(y = encounter, x = doy, col = sex_comparison)) +
+#   geom_point(size = 0.7) + 
+#   geom_smooth(method="gam", formula = y ~ s(x, bs = "cc", k = 8),
+#               method.args =list(family = poisson), se=F) +
+#   scale_x_continuous(limits = c(-2,370), expand = c(0,1)) +
+#   scale_y_continuous(limits = c(0,110), expand = c(0,1)) +
+#   scale_color_manual(values = c("#A50026", "#9970AB", "#004488"),
+#                      labels = c("Female - Female", "Female - Male", "Male - Male"),
+#                      name = "") +
+#   xlab("Day of year") +
+#   ylab("Encounter count") +
+#   ggtitle("C") +
+#   theme_bw() +
+#   theme(panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         plot.title = element_text( size = 14, family = "sans", face = "bold"),
+#         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
+#         axis.title.x = element_text(size=10, family = "sans", face = "bold"),
+#         axis.text.y = element_text(size=8, family = "sans"),
+#         axis.text.x  = element_text(size=8, family = "sans"),
+#         legend.position="none",
+#         # legend.text = element_text(size=6, family = "sans", face = "bold"),
+#         # legend.position = c(0.84, 0.2), #horizontal, vertical
+#         # legend.key.height = unit(0.3, "cm"),
+#         # legend.key=element_blank(),
+#         panel.background = element_rect(fill = "transparent"),
+#         legend.background = element_rect(fill = "transparent"),
+#         plot.background = element_rect(fill = "transparent", color = NA),
+#         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+# figure3c_encounters_overtime
+# ggsave(figure3c_encounters_overtime, width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent",
+#        file="figures/individual figures/figure3c_encounters_overtime.png")
+
+
+# #..................................................
+# # multi-panel ----
+# #..................................................
+# 
+# figure3_top <- grid.arrange(figure3a_proximity_ratio,
+#                             figure3b_encounters,
+#                             ncol = 2)
+# 
+# figure3 <- grid.arrange(figure3_top,
+#                         figure3c_encounters_overtime,
+#                         nrow = 2,
+#                         heights = c(0.35, 0.35))
+# 
+# ggsave(figure3, filename = "figures/figure3.png", device = NULL,
+#        path = NULL, scale = 1, width = 6.86, height = 6, units = "in", dpi = 600)
+
+## Plot encounter residuals ----
+# FIT_ENC <- lm(log10(encounter_count+0.1) ~ log10(overlap_est), data = proximity_pair_df)
+# resids <- residuals(FIT_ENC)
+# png(file = "figures/encounter_residuals_sex.png", width = 6.86, height = 6, units = "in", res = 600)
+# boxplot(resids ~ proximity_pair_df$sex_comparison,
+#         xlab = "Encounter Residuals",
+#         ylab = "sex")
+# dev.off()
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#alternative figure3c plot
+
 figure3c_encounters_overtime <-
-ggplot(data = encs,
-       aes(y = encounter, x = doy, col = sex_comparison)) +
-  geom_point(size = 0.7) + 
-  geom_smooth(method="gam", formula = y ~ s(x, bs = "cc", k = 8),
-              method.args =list(family = poisson), se=F) +
+  ggplot(data = encs,
+         aes(y = encounter, x = doy, col = sex_comparison)) +
+  geom_bar(stat = "identity", position = "stack") + 
+  # geom_smooth(method="gam", formula = y ~ s(x, bs = "cc", k = 8),
+  #             method.args =list(family = poisson), se=F) +
   scale_x_continuous(limits = c(-2,370), expand = c(0,1)) +
   scale_y_continuous(limits = c(0,110), expand = c(0,1)) +
   scale_color_manual(values = c("#A50026", "#9970AB", "#004488"),
@@ -140,31 +210,17 @@ figure3c_encounters_overtime
 ggsave(figure3c_encounters_overtime, width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent",
        file="figures/individual figures/figure3c_encounters_overtime.png")
 
-  
 #..................................................
-# multi-panel ----
+# multi-panel v2 ----
 #..................................................
 
 figure3_top <- grid.arrange(figure3a_proximity_ratio,
                             figure3b_encounters,
                             ncol = 2)
 
-figure3 <- grid.arrange(figure3_top,
-                        figure3c_encounters_overtime,
+figure3_v2 <- grid.arrange(figure3_top,
+                        fig3c_2,
                         nrow = 2,
                         heights = c(0.35, 0.35))
-
-ggsave(figure3, filename = "figures/figure3.png", device = NULL,
+ggsave(figure3_v2, filename = "figures/figure3_v2.png", device = NULL,
        path = NULL, scale = 1, width = 6.86, height = 6, units = "in", dpi = 600)
-
-## Plot encounter residuals ----
-# FIT_ENC <- lm(log10(encounter_count+0.1) ~ log10(overlap_est), data = proximity_pair_df)
-# resids <- residuals(FIT_ENC)
-# png(file = "figures/encounter_residuals_sex.png", width = 6.86, height = 6, units = "in", res = 600)
-# boxplot(resids ~ proximity_pair_df$sex_comparison,
-#         xlab = "Encounter Residuals",
-#         ylab = "sex")
-# dev.off()
-
-
-
