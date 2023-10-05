3d3e81a9b86a4078b6ba56c70de2031baf405c54
Kat <katchhen@gmail.com>
2023-10-05 14:30:34 +0700

    cleaned up code

--- scripts/4 proximity.R
+++ scripts/4 proximity.R
@@ -1,12 +1,8 @@
 
 # Proximity 
 #............................................................
-# Proximity ratio ----
+# Estimating proximity ratio ----
 #............................................................
-
-FIT <- readRDS("RDS/FIT.RDS")
-AKDE <- readRDS("RDS/AKDE.RDS")
-overlap_df <- readRDS("RDS/overlap_df.RDS")
 
 #subset individual movement models based on their site location
 FIT_1 <- FIT[c("Alexander", "Anthony", "Bumpus", "Cate", "Christoffer",
@@ -16,25 +12,24 @@ FIT_2 <- FIT[c("Annie", "Beto", "Hannah", "Jane", "Larry",
                "Luigi", "Margaret", "Maria", "Reid", "Sheron",
                "Thomas")]
 
-#create empty columns for the results to be saved to
+#create empty columns for results to be saved to
 overlap_1_df$proximity_low <- NA
 overlap_1_df$proximity_est <- NA
 overlap_1_df$proximity_high <- NA
 
-#calculate the proximity statistics
+#Calculate the proximity statistics
 for(i in 1:nrow(overlap_1)){
-  ANIMAL_A <- as.character(overlap_1_df[i, 'anteater_A'])
+  ANIMAL_A <- as.character(overlap_1_df[i, 'anteater_A']) # add as.character due to tibble format
   ANIMAL_B <- as.character(overlap_1_df[i, 'anteater_B'])
   
-  TRACKING_DATA.1 <- DATA_TELEMETRY[c(ANIMAL_A, ANIMAL_B)]
+  TRACKING_DATA.1 <- DATA_TELEMETRY[c(ANIMAL_A, ANIMAL_B)] # extract anteater by name, has extra layers .-. it doesnt work, that is why
+  
+  # line above is using as.character removes all the fluff because you just want the text string
   MODELS.1 <- list(FIT_1[ANIMAL_A][[1]], FIT_1[ANIMAL_B][[1]])
   
   PROXIMITY1 <- tryCatch(
     {
-      #calculate the proximity statistic
-      PROXIMITY_1 <- proximity(data = TRACKING_DATA.1, 
-                               CTMM = MODELS.1, 
-                               GUESS=ctmm(error=FALSE))},
+      PROXIMITY_1 <- proximity(data = TRACKING_DATA.1, CTMM = MODELS.1, GUESS=ctmm(error=FALSE))},
     error=function(err){
       PROXIMITY_1 <- c(NA,NA,NA)
       return(PROXIMITY_1)
@@ -47,24 +42,26 @@ for(i in 1:nrow(overlap_1)){
   
   #save results to a csv file
   write.csv(overlap_1_df, "data/DATA_proximity_1.csv", row.names = FALSE)
-  #track progress
-  cat("finished index", i, "\n")
+  
+  cat("finished index", i, "\n") # see the loop happening in real time
 }
 
+#.....
 
-#create empty columns for the results to be saved to
+#create empty columns for results to be saved to
 overlap_2_df$proximity_low <- NA
 overlap_2_df$proximity_est <- NA
 overlap_2_df$proximity_high <- NA
 
-#calculate the proximity statistics
+#Calculate the proximity statistics
 for(i in 1:nrow(overlap_2_df)){
-  ANIMAL_A <- as.character(overlap_2_df[i, 'anteater_A'])
+  ANIMAL_A <- as.character(overlap_2_df[i, 'anteater_A']) # add as.character due to tibble format
   ANIMAL_B <- as.character(overlap_2_df[i, 'anteater_B'])
   
-  TRACKING_DATA_2 <- DATA_TELEMETRY[c(ANIMAL_A, ANIMAL_B)] 
-  MODELS_2 <- list(FIT_2[ANIMAL_A][[1]], FIT_2[ANIMAL_B][[1]])
+  TRACKING_DATA_2 <- DATA_TELEMETRY[c(ANIMAL_A, ANIMAL_B)] # extract anteater by name, has extra layers .-. it doesnt work, that is why
   
+  # line above is using as.character removes all the fluff because you just want the text string
+  MODELS_2 <- list(FIT_2[ANIMAL_A][[1]], FIT_2[ANIMAL_B][[1]])
   PROXIMITY_2 <- tryCatch(
     {
       #calculate the proximity statistic
@@ -82,12 +79,12 @@ for(i in 1:nrow(overlap_2_df)){
   
   #save results in a csv file
   write.csv(overlap_2_df, "data/DATA_proximity_2.csv", row.names = FALSE)
-  #track progress
-  cat("finished index", i, "\n")
+  cat("finished index", i, "\n") # see the loop happening in real time
 }
 
 #............................................................
-#import proximity statistics data
+
+#import proximity data
 DATA_proximity_1 <- read_csv("data/DATA_proximity_1.csv")
 DATA_proximity_2 <- read_csv("data/DATA_proximity_2.csv")
 
@@ -101,43 +98,35 @@ DATA_proximity_2$anteater_B[DATA_proximity_2$anteater_B == "Larry 267"] <- "Larr
 DATA_proximity_2$site <- 2
 DATA_proximity_2 <- relocate(DATA_proximity_2, site, .before = proximity_low)
 
-#create a proximity dataframe
 proximity_df <- bind_rows(DATA_proximity_1, DATA_proximity_2)
 proximity_df <- proximity_df[,-3]
 proximity_df <- mutate(proximity_df,
-                       sex_comparison = 
-                         case_when(paste(Sex.A, Sex.B) == "Male Male" ~ "male-male",
-                                   paste(Sex.A, Sex.B) == "Female Female" ~ "female-female",
-                                   paste(Sex.A, Sex.B) == "Male Female" ~ "female-male",
-                                   paste(Sex.A, Sex.B) == "Female Male" ~ "female-male"))
+                       sex_comparison = case_when(paste(Sex.A, Sex.B) == "Male Male" ~ "male-male",
+                                                  paste(Sex.A, Sex.B) == "Female Female" ~ "female-female",
+                                                  paste(Sex.A, Sex.B) == "Male Female" ~ "female-male",
+                                                  paste(Sex.A, Sex.B) == "Female Male" ~ "female-male"))
 
 #add home range overlap data to proximity dataframe
-proximity_df <- left_join(overlap_df, proximity_df, 
-                          by = c("anteater_A", "anteater_B",
-                                 "Sex.A", "Sex.B",
-                                 "Age.A", "Age.B",
-                                 "sex_comparison",
-                                 "site"))
+proximity_df <- left_join(overlap_df, proximity_df, by = c("anteater_A", "anteater_B",
+                                                           "Sex.A", "Sex.B",
+                                                           "Age.A", "Age.B",
+                                                           "sex_comparison",
+                                                           "site"))
 
 #save proximity dataframe
 saveRDS(proximity_df, file = "rds/proximity_df.rds")
 
 #............................................................
-# Proximity ratio results
+# Proximity ratio results ----
 #............................................................
 
-#test for significance in sex, compare model with and without sex as a variable
-proximity_test <- glmer(proximity_est ~ sex_comparison + (1|site), 
-                        family = Gamma(link = "log"), data = proximity_df)
-proximity_test2 <- glmer(proximity_est ~ 1 + (1|site), 
-                         family = Gamma(link = "log"), data = proximity_df)
+#test for significance in sex, compare model with and without sex as a variable across all 121 dyads
+proximity_test <- glmer(proximity_est ~ sex_comparison + (1|site), family = Gamma(link = "log"), data = proximity_df)
+proximity_test2 <- glmer(proximity_est ~ 1 + (1|site), family = Gamma(link = "log"), data = proximity_df)
 proximity_test_results <- anova(proximity_test, proximity_test2)
-proximity_test_pvalue <- round(proximity_test_results$`Pr(>Chisq)`[2], 2)
+proximity_test_pvalue <- round(proximity_test_results$`Pr(>Chisq)`[2], 2) #p = 0.13
 
-#test for significance in home range overlap, compare model with and without overlap as a variable
-prox_overlap_test <- glmer(proximity_est ~ overlap_est + (1|site), 
-                           family = Gamma(link = "log"), data = proximity_df)
-prox_overlap_test2 <- glmer(proximity_est ~ 1 + (1|site), 
-                            family = Gamma(link = "log"), data = proximity_df)
+prox_overlap_test <- glmer(proximity_est ~ overlap_est + (1|site), family = Gamma(link = "log"), data = proximity_df)
+prox_overlap_test2 <- glmer(proximity_est ~ 1 + (1|site), family = Gamma(link = "log"), data = proximity_df)
 prox_overlap_test_results <- anova(prox_overlap_test, prox_overlap_test2)
-prox_overlap_test_pvalue <- round(prox_overlap_test_results$`Pr(>Chisq)`[2], 2)
+prox_overlap_test_pvalue <- round(prox_overlap_test_results$`Pr(>Chisq)`[2], 2) #p = 0.03
