404904037b7733a0b72c8b9b71dd5059fff6fcf7
Kat <katchhen@gmail.com>
2023-10-05 14:59:17 +0700

    investigated December 2018 encounter counts

--- scripts/7 case study.R
+++ scripts/7 case study.R
@@ -35,7 +35,43 @@ round(mean(distance_pairs_df$est[distance_pairs_df$pair_ID_number == 11])/1000, 
 round(min(distance_pairs_df$est[distance_pairs_df$pair_ID_number == 11])/1000, 2)
 round(max(distance_pairs_df$est[distance_pairs_df$pair_ID_number == 11])/1000, 2)
 
-#calculate the mean 95% CI correlative movement
+#investigate when encounter events occurred
+
+#subset distance data
+distance_pair11 <- distance_pairs_df[distance_pairs_df$pair_ID_number == 11,]
+
+#visualization of the distances between the two over time
+ggplot() +
+  geom_line(data = distance_pair11,
+            aes(y = est, x = timestamp), 
+            size = 0.15) +
+  xlab("") +
+  ylab("Distance (m)") +
+  scale_x_datetime(labels = scales::date_format("%b %Y"))
+
+#clustering in late October is observed
+distance_pair11$month <- format(distance_pair11$timestamp, "%m")
+distance_pair11$day <- format(distance_pair11$timestamp, "%d")
+distance_pair11$date <- format(distance_pair11$timestamp, "%Y-%m-%d")
+
+#subset October data with a threshold of 15m to take a closer look
+oct <- distance_pair11[distance_pair11$month == 10,]
+oct <- oct[oct$est < 15,]
+oct$month <- format(oct$timestamp, "%b")
+oct$date <- format(oct$timestamp, "%b %d")
+
+#when did the encounter events occur in October?
+table(oct$day)
+
+#visualization of events
+ggplot() +
+  geom_line(data = oct,
+            aes(y = est, x = timestamp, 
+            ), size = 0.15) +
+  xlab("") +
+  ylab("Distance (m)")
+
+#calculate the mean 95% CI correlated movement
 round(mean(cm_pair11$etaTot.CI.Low), 2)
 round(mean(cm_pair11$etaTot.MLE), 2)
 round(mean(cm_pair11$etaTot.CI.Upp), 2)
