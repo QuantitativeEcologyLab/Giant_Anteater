d3f254446bf19cf74d96da325efec2e25a1d8ad6
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-02-11 18:54:12 -0800

    moved home-range vs weight analysis to separate script file

--- scripts/save_extract_hr.R
+++ scripts/save_extract_hr.R
@@ -146,25 +146,4 @@ dat.hr$ID[dat.hr$ID == "Little Rick"] <- "Little_Rick"
 dat.hr <- merge(dat.hr, bio_data, all = TRUE)
 
 #saveRDS(dat.hr, file = "data/home_range/dat_hr.rds")
-dat.hr <- ("data/home_range/dat_hr.rds")
-
-
-
-
-#............................................................
-# Is weight a factor in home-range size?
-
-#compare model with and without weight as a variable
-HR_test <- glmmTMB(HR_est ~ Weight + (1|Site), 
-                    family = Gamma(link = "logit"), data = dat.hr)
-HR_test2 <- glmmTMB(HR_est ~ 1 + (1|Site), 
-                     family = Gamma(link = "logit"), data = dat.hr)
-
-HR_test_results <- anova(HR_test, HR_test2)
-HR_test_results
-
-#calculate weight impact via likelihood ratio test
-HR_test_pvalue <- round(HR_test_results$`Pr(>Chisq)`[2], 2)
-HR_test_pvalue
-
-
+dat.hr <- readRDS("data/home_range/dat_hr.rds")
